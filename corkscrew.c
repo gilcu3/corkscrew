f72be3b7f719ddfb549a7fefee3245979eab7610
Reynaldo <828241+gilcu3@users.noreply.github.com>
2017-12-11 17:09:41 -0500

    added proper support for qop directive fixed nasty socket patch not working in some squid proxy when the http content was sufficiently long

--- corkscrew.c
+++ corkscrew.c
@@ -35,7 +35,7 @@ int main PARAMS((int argc, char *argv[]));
 /*
 char linefeed[] = "\x0A\x0D\x0A\x0D";
 */
-char linefeed[] = "\r\n\r\n"; /* it is better and tested with oops & squid */
+char linefeed[] = "\r\n"; /* it is better and tested with oops & squid */
 
 /*
 ** base64.c
@@ -251,15 +251,13 @@ char* digest_authentication_encode(char* user, char* realm, char* passwd, char *
 		len += strlen(cnonce);
 	}
 
-
-
 	char *res = (char*)malloc(len);
-	sprintf(res, "username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", response=\"%s\", qop=\"%s\", nc=%s, cnonce=\"%s\"",
+	if(qop)
+		sprintf(res, "username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", response=\"%s\", qop=%s, nc=%s, cnonce=\"%s\"",
 			user, realm, nonce, path, response, qop, nc, cnonce);
-	
-
-
-
+	else
+		sprintf(res, "username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", response=\"%s\"",
+			user, realm, nonce, path, response);
 	
 	return res;
 }
@@ -309,7 +307,16 @@ proxyauth * get_param(char * buffer){
 	
 }
 
-
+char * header(char * buffer){
+	char * where = strstr(buffer, "\r\n\r\n");
+	if(where==NULL)return "";
+	else{
+		char *head = (char*)malloc(256);
+		strncat(head, buffer, where - buffer);
+		head[where - buffer] = '\0';
+		return head;
+	}
+}
 
 
 #ifdef ANSI_FUNC
@@ -333,13 +340,15 @@ char *argv[];
 	ssize_t len;
 	FILE *fp;
 	char line[BUFSIZE];
+	char uri_path[256] = "";
 	int debug;
 	int n_file_descriptor;
 	int nc = 1;
 	char prev_nonce[33] = "";
+	int reading = 0;
 	
 	debug = 0;
-	port = 80;
+	port = 443;
 
 	if ((argc == 5) || (argc == 6)) {
 		if (argc == 5) {
@@ -369,13 +378,25 @@ char *argv[];
 		usage();
 		exit(-1);
 	}
-
-	strncpy(uri, "CONNECT ", sizeof(uri));
-	strncat(uri, desthost, sizeof(uri) - strlen(uri) - 1);
-	strncat(uri, ":", sizeof(uri) - strlen(uri) - 1);
-	strncat(uri, destport, sizeof(uri) - strlen(uri) - 1);
-	strncat(uri, " HTTP/1.0", sizeof(uri) - strlen(uri) - 1);
-	strncat(uri, linefeed, sizeof(uri) - strlen(uri) - 1);
+	memset(uri, 0, sizeof(uri));
+	
+	memset(uri_path, 0, sizeof(uri_path));
+	strcat(uri_path, desthost);
+	strcat(uri_path, ":");
+	strcat(uri_path, destport);
+	
+	strcat(uri, "CONNECT ");
+	strcat(uri, uri_path);
+	strcat(uri, " HTTP/1.1");
+	strcat(uri, linefeed);
+	
+	strcat(uri, "Host: ");
+	strcat(uri, uri_path);
+	strcat(uri, linefeed);
+	
+	strcat(uri, "Proxy-Connection: Keep-Alive");
+	strcat(uri, linefeed);
+	strcat(uri, linefeed);
 
 	csock = sock_connect(host, port);
 	if(csock == -1) {
@@ -405,13 +426,17 @@ char *argv[];
 		if (setup == 0) {
 			
 			if (FD_ISSET(csock, &rfd)) {
+				
 				len = read(csock, buffer, sizeof(buffer));
-
+				
 				if (len <= 0)	break;
 				else {
+					if(reading == 1)continue;
+					reading = 1;
 					sscanf(buffer,"%s%d%[^\n]",version,&code,descr);
 					if(debug & 1)fprintf(stderr, "DEBUG: version = %s code = %d\n", version, code);
-					//if(debug & 1)fprintf(stderr, "DEBUG: header = %s\n", buffer);
+					if(debug & 1)fprintf(stderr, "DEBUG: header = %s\n", header(buffer));
+					if(debug & 1)fprintf(stderr, "DEBUG: len = %d\n", len);
 					if ((strncmp(version,"HTTP/",5) == 0) && (code >= 200) && (code < 300))
 						setup = 1;
 					else {
@@ -430,26 +455,33 @@ char *argv[];
 									fprintf(stderr, "Error: Proxy response contains errors (nonce)\n");
 									exit(-1);
 								}
+								memset(uri, 0, sizeof(uri));
 								
-								strncpy(uri, "CONNECT ", sizeof(uri));
-								strncat(uri, desthost, sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, ":", sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, destport, sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, " HTTP/1.0", sizeof(uri) - strlen(uri) - 1);
+								strcat(uri, "CONNECT ");
+								strcat(uri, uri_path);
+								strcat(uri, " HTTP/1.1");
+								strcat(uri, linefeed);
+								
+								strcat(uri, "Host: ");
+								strcat(uri, uri_path);
+								strcat(uri, linefeed);
+								
+								strcat(uri, "Proxy-Connection: Keep-Alive");
+								strcat(uri, linefeed);
+								
 								if ((argc == 6) || (argc == 7)) {
 									char user[256] = "";
 									sscanf(up, "%[^:]", user);
 									char passwd[256] = "";
 									sscanf(up + strlen(user) + 1, "%s", passwd);
 									char method[32] = "CONNECT";
-									char uri_path[32] = "/";
 									
-									strncat(uri, "\nProxy-Authorization: Digest ", sizeof(uri) - strlen(uri) - 1);
+									strcat(uri, "Proxy-Authorization: Digest ");
 									
 									char* auth_string;
 									
 									if(strncmp(pa->qop, "auth", 5) == 0){
-										/* prepare an random string for cnounce */
+										/* prepare a random string for cnounce */
 										char cnonce[17];
 										snprintf(cnonce, sizeof(cnonce), "%08x%08x", red_randui32(), red_randui32());
 										
@@ -458,16 +490,13 @@ char *argv[];
 											strncpy(prev_nonce, pa->nonce, 32);
 											nc = 1;
 										}
+										
 										char str_nc[17];
 										snprintf(str_nc, sizeof(str_nc), "%08x", nc);
-										
-										
-										
 										
 										auth_string = digest_authentication_encode(
 												user, pa->realm, passwd, //user, realm, pass
 												method, uri_path, str_nc, pa->nonce, cnonce, pa->qop); // method, path, nc, cnonce, qop (for now fixed)
-											
 									}
 									else{
 										auth_string = digest_authentication_encode(
@@ -475,79 +504,63 @@ char *argv[];
 												method, uri_path, NULL, pa->nonce, NULL, NULL); // method, path, nc, cnonce, qop (for now fixed)
 										
 									}
+									strcat(uri, auth_string);
+									strcat(uri, linefeed);
 									
+									//char user_agent[256] = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0";
+									//strncat(uri, user_agent, sizeof(uri) - strlen(uri) - 1);
 									
-									
-									
-									strncat(uri, auth_string, sizeof(uri) - strlen(uri) - 1);
-									
-									strncat(uri, linefeed, sizeof(uri) - strlen(uri) - 1);
-									if(debug & 1)fprintf(stderr, "DEBUG: %s\n", uri);
+									strcat(uri, linefeed);
 								}
 								else{
 									fprintf(stderr, "Error: User and password missing\n");
 									exit(-1);
 								}
 								
-								
-								
-								
-								
-								csock = sock_connect(host, port);
-								if(csock == -1) {
-									fprintf(stderr, "Error: Couldn't establish connection to proxy: %s\n", strerror(errno));
-									exit(-1);
-								}
 								sent = 0;
-								if(debug & 1)fprintf(stderr, "DEBUG: done\n");
-								continue;
+								if(debug & 1)fprintf(stderr, "DEBUG: done digest\n");
 								
 							}
 							else if(strstr(buffer, "Basic realm") != NULL){
 								if(debug & 1)fprintf(stderr, "DEBUG: Basic auth needed\n");
-								strncpy(uri, "CONNECT ", sizeof(uri));
-								strncat(uri, desthost, sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, ":", sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, destport, sizeof(uri) - strlen(uri) - 1);
-								strncat(uri, " HTTP/1.0", sizeof(uri) - strlen(uri) - 1);
+								
+								memset(uri, 0, sizeof(uri));
+								
+								strcat(uri, "CONNECT ");
+								strcat(uri, uri_path);
+								strcat(uri, " HTTP/1.1");
+								strcat(uri, linefeed);
+								
 								if ((argc == 6) || (argc == 7)) {
-									strncat(uri, "\nProxy-Authorization: Basic ", sizeof(uri) - strlen(uri) - 1);
-									strncat(uri, base64_encode(up), sizeof(uri) - strlen(uri) - 1);
+									strcat(uri, "Proxy-Authorization: Basic ");
+									strcat(uri, base64_encode(up));
 								}
 								else{
 									fprintf(stderr, "Error: User and password missing\n");
 									exit(-1);
 								}
-								strncat(uri, linefeed, sizeof(uri) - strlen(uri) - 1);
-								csock = sock_connect(host, port);
-								if(csock == -1) {
-									fprintf(stderr, "Error: Couldn't establish connection to proxy: %s\n", strerror(errno));
-									exit(-1);
-								}
-								
-								
+								strcat(uri, linefeed);
+								strcat(uri, linefeed);
 								
 								sent = 0;
-								continue;
-								
 							}
 							
-							fprintf(stderr, "Error: Proxy could not open connnection to %s: %s\n", desthost, descr);
-							exit(-1);
+							else{
+								fprintf(stderr, "Error: Proxy could not open connnection to %s: %s\n", desthost, descr);
+								exit(-1);
+							}
 							
-							
+							continue;
 						}
-						
 						fprintf(stderr, "Error: Proxy could not open connnection to %s: %s\n", desthost, descr);
-						//if(debug & 1)fprintf(stderr, "%s\n", buffer);
 						exit(-1);
-		
 					}
 				}
 			}
 			if (FD_ISSET(csock, &sfd) && (sent == 0)) {
+				reading = 0;
 				len = write(csock, uri, strlen(uri));
-				if(debug & 1)fprintf(stderr, "DEBUG: uri = %s\n", uri);
+				if(debug & 1)fprintf(stderr, "DEBUG: uri = \n%s\n", uri);
 				if (len <= 0)
 					break;
 				else{
